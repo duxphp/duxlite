cb5f7b802cf9805ec201d02749644dba9636770a
duxphp <admin@duxphp.com>
2023-02-20 15:54:28 +0800

    update

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import Unocss from 'duxweb-cli/plugins/unocss'
+import duxweb from 'duxweb-cli/plugins/duxweb'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: resolve(__dirname, 'client'),
+  plugins: [
+    react(),
+    duxweb(),
+    Unocss()
+  ],
+  publicDir: false,
+  resolve: {
+    alias: {
+      '@/app': resolve(__dirname, 'client/app')
+    }
+  },
+  build: {
+    outDir: resolve(__dirname, 'public')
+  }
+})
